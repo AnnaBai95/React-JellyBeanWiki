84f218edbbbdd1894ab64b0dfd22633487b09bed
AnnaBai95 <annastacia_bailey@yahoo.com>
2024-05-28 23:09:43 -0500

    Api call + interface + Tailwind + Scss
    Added the api call to retrieve the jelly bean list and started to create an interface. Also added tailwind and Sass

--- jelly-bean-wiki/src/App.js
+++ jelly-bean-wiki/src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [jellyResponse, setJellyResponse] = useState({});
+  const [jellyList, setJellyList] = useState([]);
+
+  useEffect(() => {
+    
+    fetch("https://jellybellywikiapi.onrender.com/api/beans")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setJellyResponse(data);
+        setJellyList(jellyResponse.items);
+      })
+      .catch((error) => {
+        console.log("Error", error);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div className="grid grid-cols-1 md:grid-col-2 lg:grid-cols-3 xl:grid-cols-4">
+        {jellyList.map((jellyBean, index) => (
+          <a href="#">
+            <div className="card" key={index}>
+              <img
+                src={jellyBean.imageUrl}
+                alt={`${jellyBean.flavorName} jellybean`}
+              ></img>
+              <div>
+                <p>{jellyBean}</p>
+              </div>
+            </div>
+          </a>
+        ))}
+      </div>
+    </main>
   );
 }
 
---
+++ jelly-bean-wiki/src/interfaces/jellyBean.js
@@ -0,0 +1,3 @@
+interface jellyBean{
+    beanId:number
+}
