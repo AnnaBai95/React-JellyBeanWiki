f129540861b2008f628d81f575a7f6fbdf3c1fb9
AnnaBai95 <annastacia_bailey@yahoo.com>
2024-07-16 20:52:15 -0500

    Animated skeleton
    Started on the animated skeleton

--- jelly-bean-wiki/src/App.js
+++ jelly-bean-wiki/src/App.js
@@ -4,6 +4,7 @@ import jellyWikiResponse from "./interfaces/jellyBean.ts";
 import NavBar from "./components/navigation/NavBar.js";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCheckCircle } from "@fortawesome/free-regular-svg-icons";
+import { faBan } from "@fortawesome/free-solid-svg-icons";
 import Pagination from "./components/navigation/Pagination.js";
 import SearchBar from "./components/others/SearchBar.js";
 
@@ -57,11 +58,11 @@ function App() {
       });
   };
 
-    useEffect(() => {
+  useEffect(() => {
     console.log("Get res", jellyResponse);
   }, [jellyResponse]);
 
-    useEffect(() => {
+  useEffect(() => {
     console.log("List", jellyList);
   }, [jellyList]);
 
@@ -70,7 +71,31 @@ function App() {
       <NavBar></NavBar>
       <main className="2xl:container mx-auto px-6 mt-16">
         <SearchBar onFlavorSearch={handleFlavorSearch}></SearchBar>
+
         <div className="grid grid-cols-1 xs:grid-col-2  sm:grid-col-2 md:grid-col-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 mb-10 mt-6">
+       
+               {/* {jellyList.length < 0 && ( */}
+               <div className="card rounded-md shadow-lg bg-white relative animate-pulse">
+            <div className="card-header rounded-t-md rounded-r-md">
+              <svg
+                class="w-10 h-10 text-gray-200 dark:text-gray-600"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                viewBox="0 0 20 18"
+              >
+                <path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z" />
+              </svg>
+            </div>
+            <div className="card-body p-4">
+              <p className="mb-3 h-4 bg-gray-200 rounded-full dark:bg-gray-700"></p>
+              <p className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700"></p>
+              <div className="mt-10">
+                <p className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700text-gray-800 text-sm font-semibold me-2"></p>
+              </div>
+            </div>
+          </div>
+        {/* )} */}
           {jellyList.map((jellyBean, index) => (
             <a href="#" key={index}>
               <div className="card rounded-md shadow-lg bg-white relative">
@@ -157,6 +182,16 @@ function App() {
           totalPages={jellyResponse.totalPages}
           onPageChange={handlePageChange}
         ></Pagination>
+
+        {/* {jellyList.length < 1 && (
+          <div className="text-center">
+            <FontAwesomeIcon
+              icon={faBan}
+              className="text-5xl"
+            ></FontAwesomeIcon>
+            <p className="text-4xl">There are no jelly beans to be displayed</p>
+          </div>
+        )} */}
       </main>
     </div>
   );
