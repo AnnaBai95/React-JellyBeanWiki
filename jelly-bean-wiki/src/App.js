03cd367af2fb9a40b185fcdf773bc4c04be666ed
AnnaBai95 <annastacia_bailey@yahoo.com>
2024-07-24 21:28:05 -0500

    Update App.js
    Show the jelly message only if I search has been done. Continued working on the loading skeleton

--- jelly-bean-wiki/src/App.js
+++ jelly-bean-wiki/src/App.js
@@ -13,15 +13,20 @@ function App() {
   const [jellyList, setJellyList] = useState([]);
   const [pageIndex, setPageIndex] = useState(1);
   const [pageSize, setPageSize] = useState(12);
+  const [loading, setLoading] = useState(false);
+  const [showMessage, setShowMessage] = useState(false);
+  const [hasSearched, setHasSearched] = useState(false);
 
   useEffect(() => {
     fetch(
       `https://jellybellywikiapi.onrender.com/api/beans?pageIndex=${pageIndex}&pageSize=${pageSize}`
     )
       .then((response) => {
+        setLoading(true);
         return response.json();
       })
       .then((data) => {
+        setLoading(false);
         setJellyResponse(data);
         setJellyList(data.items);
       })
@@ -29,14 +34,6 @@ function App() {
         console.log("Error", error);
       });
   }, [pageIndex, pageSize]);
-
-  // useEffect(() => {
-  //   console.log("Get res", jellyResponse);
-  // }, [jellyResponse]);
-
-  // useEffect(() => {
-  //   console.log("List", jellyList);
-  // }, [jellyList]);
 
   const handlePageChange = (pageNumber) => {
     setPageIndex(pageNumber);
@@ -50,6 +47,7 @@ function App() {
         return response.json();
       })
       .then((data) => {
+        setHasSearched(true);
         setJellyResponse(data);
         setJellyList(data.items);
       })
@@ -59,12 +57,12 @@ function App() {
   };
 
   useEffect(() => {
-    console.log("Get res", jellyResponse);
-  }, [jellyResponse]);
-
-  useEffect(() => {
-    console.log("List", jellyList);
-  }, [jellyList]);
+    if (hasSearched && jellyList.length < 1) {
+      setShowMessage(true);
+    } else {
+      setShowMessage(false);
+    }
+  }, [hasSearched, jellyList]);
 
   return (
     <div className="mb-16">
@@ -73,29 +71,28 @@ function App() {
         <SearchBar onFlavorSearch={handleFlavorSearch}></SearchBar>
 
         <div className="grid grid-cols-1 xs:grid-col-2  sm:grid-col-2 md:grid-col-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 mb-10 mt-6">
-       
-               {/* {jellyList.length < 0 && ( */}
-               <div className="card rounded-md shadow-lg bg-white relative animate-pulse">
-            <div className="card-header rounded-t-md rounded-r-md">
-              <svg
-                class="w-10 h-10 text-gray-200 dark:text-gray-600"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                viewBox="0 0 20 18"
-              >
-                <path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z" />
-              </svg>
-            </div>
-            <div className="card-body p-4">
-              <p className="mb-3 h-4 bg-gray-200 rounded-full dark:bg-gray-700"></p>
-              <p className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700"></p>
-              <div className="mt-10">
-                <p className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700text-gray-800 text-sm font-semibold me-2"></p>
+          {loading && (
+            <div className="card rounded-md shadow-lg bg-white relative animate-pulse">
+              <div className="card-header rounded-t-md rounded-r-md">
+                <svg
+                  className="w-10 h-10 text-gray-200 dark:text-gray-600"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 20 18"
+                >
+                  <path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z" />
+                </svg>
+              </div>
+              <div className="card-body p-4">
+                <p className="mb-6 h-4 bg-gray-200 rounded-full dark:bg-gray-700"></p>
+                <p className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700"></p>
+                <div className="mt-20">
+                  <p className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 font-semibold me-2"></p>
+                </div>
               </div>
             </div>
-          </div>
-        {/* )} */}
+          )}
           {jellyList.map((jellyBean, index) => (
             <a href="#" key={index}>
               <div className="card rounded-md shadow-lg bg-white relative">
@@ -183,15 +180,20 @@ function App() {
           onPageChange={handlePageChange}
         ></Pagination>
 
-        {/* {jellyList.length < 1 && (
+        {showMessage && (
           <div className="text-center">
             <FontAwesomeIcon
               icon={faBan}
               className="text-5xl"
             ></FontAwesomeIcon>
-            <p className="text-4xl">There are no jelly beans to be displayed</p>
+            <p className="text-4xl mb-6">There are no jelly beans to be displayed</p>
+            <p className="text-2xl">
+              Please ensure that you type the full name of the jelly bean you
+              wish to find. Example "Barbados Cherry". Partial searches example
+              "Bar", "Barbados" will not return a result.
+            </p>
           </div>
-        )} */}
+        )}
       </main>
     </div>
   );
