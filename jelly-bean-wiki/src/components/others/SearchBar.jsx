26dd8906537cc1edfee94075ec7f63bd4c564de8
AnnaBai95 <annastacia_bailey@yahoo.com>
2024-10-07 22:48:11 -0500

    Mobile pagination + Search box + Demo
    Created a mobile pagination system.
    Added a x button to clear the search results in the text box and search results
    Created a new screen recording

--- jelly-bean-wiki/src/components/others/SearchBar.jsx
+++ jelly-bean-wiki/src/components/others/SearchBar.jsx
@@ -1,20 +1,36 @@
-import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { faSearch, faX } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "../../scss/components/form_controls.scss";
 
-function SearchBar({ onFlavorSearch }) {
+function SearchBar({
+  onFlavorSearch,
+  hasSearched,
+  onClearSearchValue,
+  flavorSearched,
+}) {
   return (
     <div className="flex flex-col w:full lg:w-2/4 me-4">
-      <label htmlFor="flavorSearch" className="font-semibold">Search for jellybean flavors using the exact flavor name</label>
+      <label htmlFor="flavorSearch" className="font-semibold">
+        Search for jellybean flavors using the exact flavor name
+      </label>
       <div className="input-group w-full">
-      <FontAwesomeIcon
+        <FontAwesomeIcon
           icon={faSearch}
           className="search-icon me-5"
         ></FontAwesomeIcon>
         <input
           onChange={(e) => onFlavorSearch(e.target.value)}
           id="flavorSearch"
+          value={flavorSearched}
         ></input>
+        {hasSearched && flavorSearched !== "" && (
+          <button onClick={() => onClearSearchValue()}>
+            <FontAwesomeIcon
+              icon={faX}
+              className="ms-5 search-icon"
+            ></FontAwesomeIcon>
+          </button>
+        )}
       </div>
     </div>
   );
